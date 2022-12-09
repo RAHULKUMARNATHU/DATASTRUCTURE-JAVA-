04e59a4004463624fca2147dc7eeab42aac89ab9
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-12-10 00:22:51 +0530

    commit :: variables

---
+++ Basics/AddTwoNumbers.java
@@ -0,0 +1,8 @@
+public class AddTwoNumbers{
+public static void main(String[] args) {
+    int a =10;
+    int b=25;
+    System.out.println("sum of a and b " + (a+b));
+    // System.out.println(a+b);
+}
+}
