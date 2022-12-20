872518570b797cd2cb33b5eab58c7afd119a69d2
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-12-20 23:18:22 +0530

    commit :: Inverted Star Pattern

--- Patterns2/InvertedTrianglePattern.java
+++ Patterns2/InvertedTrianglePattern.java
@@ -1,13 +1,12 @@
-
 /*Code : Mirror Image Number Pattern
 Send Feedback
 Print the following pattern for the given N number of rows.
 Pattern for N = 4
 
-...*
-..**
-.***
 ****
+***.
+**..
+*...
 The dots represent spaces.
 
 
@@ -17,26 +16,29 @@ Output format :
 Pattern in N lines
  */ 
 
+
 import java.util.Scanner;
 
-public class MirrorImageStarPattern {
+public class InvertedTrianglePattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc .nextInt();
-        int i = 1;
-        while(i<=n){
-            int spaces = 1;
-            while(spaces<=n-i){
-                System.out.print(" ");
-                spaces+=1;
+        int n = sc.nextInt();
+        int i = 0;
+
+        while(i <n){
+            
+            int star=1;
+            while(star<=n-i){
+                System.out.print("*");
+                star++;
             }
             int j =1;
-            while(j<=i){
-
-                System.out.print("*");
-            j++;
+            while(j<i){
+                System.out.print(" ");
+                j++;
             }
             System.out.println();
+            
             i++;
         }
     }
