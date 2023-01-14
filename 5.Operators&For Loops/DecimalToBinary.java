450c3e09b0a25aa59b528358d467030428798c99
RAHUL KUMAR <rahulkumars9572@gmail.com>
2023-01-14 15:49:07 +0530

    commit :: reference

--- 5.Operators&For Loops/DecimalToBinary.java
+++ 5.Operators&For Loops/DecimalToBinary.java
@@ -1,3 +1,26 @@
+/*Decimal to Binary
+Send Feedback
+Given a decimal number (integer N), convert it into binary and print.
+The binary number should be in the form of an integer.
+Note: The given input number could be large, so the corresponding binary number can exceed the integer range. So you may want to take the answer as long for CPP and Java.
+
+
+Note for C++ coders: Do not use the inbuilt implementation of "pow" function. The implementation of that function is done for 'double's and it may fail when used for 'int's, 'long's, or 'long long's. Implement your own "pow" function to work for non-float data types.
+
+
+Input format :
+Integer N
+Output format :
+Corresponding Binary number (long)
+Constraints :
+0 <= N <= 10^5
+Sample Input 1 :
+12
+Sample Output 1 :
+1100
+ */
+
+
 import java.util.Scanner;
 
 public class DecimalToBinary {
