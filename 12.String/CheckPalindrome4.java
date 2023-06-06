657747a3d2d3db6612ff67620cc65bb069060640
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-06-06 17:20:22 +0530

    commit :: optimized palindrome

--- 12.String/CheckPalindrome4.java
+++ 12.String/CheckPalindrome4.java
@@ -1,21 +1,42 @@
 import java.util.Scanner;
 
 public class CheckPalindrome4 {
-    public static Boolean checkPalindrome(String str){
+    // public static Boolean checkPalindrome(String str){
         
-        String revString = "";
-        int length = str.length();
-        if(length ==0){
-            return false;
+    //     String revString = "";
+    //     int length = str.length();
+    //     if(length ==0){
+    //         return false;
+    //     }
+    //     for(int i =0 ; i<length ; i++){
+    //         revString = str.charAt(i)+revString;
+    //     }
+    //     if(revString.equals(str)){
+    //         return true;
+    //     }       
+    //     return false;
+    // }
+
+
+    /*Optimized */
+    public static boolean checkPalindrome(String str){
+
+        int i = 0 ;
+        int j = str.length()-1;
+
+        while(i<=j){
+            if(str.charAt(i)!=str.charAt(j)){
+             return false;  
+            }
+            i++;
+            j--;
         }
-        for(int i =0 ; i<length ; i++){
-            revString = str.charAt(i)+revString;
-        }
-        if(revString.equals(str)){
-            return true;
-        }       
-        return false;
-    }
+
+        return true;
+    }  
+  
+  
+  
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
