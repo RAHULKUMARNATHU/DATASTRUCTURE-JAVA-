f48fd9214d586d28a1df7a19ad80c5bfd789e0dd
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-06-09 16:22:39 +0530

    commit :: print all substring

--- 12.String/PrintAllSubString5.java
+++ 12.String/PrintAllSubString5.java
@@ -24,14 +24,22 @@ public class PrintAllSubString5 {
         //     }
         // }
 
-        for(int i = 0 ; i<= str.length() ; i++){
-            int start = 0 ;
-            for(int end = i ; end < str.length() ; end ++){
-                System.out.println(str.substring(start, end+1));
-                start++;
-            }
+        // for(int i = 0 ; i<= str.length() ; i++){
+        //     int start = 0 ;
+        //     for(int end = i ; end < str.length() ; end ++){
+        //         System.out.println(str.substring(start, end+1));
+        //         start++;
+        //     }
            
-        }
+        // }
+
+            for( int len = 1  ; len<= str.length() ; len++ ){
+                for(int start = 0 ; start<=str.length() - len ; start ++){
+                    int end = start + len -1;
+                    System.out.println(str.substring(start, end+1 ));
+                }
+            }
+
     
     }
     public static void main(String[] args) {
