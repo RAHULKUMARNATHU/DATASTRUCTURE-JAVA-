68bf3f412317a8197bfd15081765ec5352288f6a
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-07-13 00:05:42 +0530

    commit :: factorial using recursion

---
+++ 14.Recursion/printFactorial.java
@@ -0,0 +1,18 @@
+/*Find factorial */
+
+import java.util.Scanner;
+
+public class printFactorial{
+    public static int findFactorial(int n ){
+        if(n ==0){
+            return 1;
+        }
+       return n * findFactorial(n-1);
+    }
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int res = findFactorial(n);
+    System.out.println(res);
+}
+}
