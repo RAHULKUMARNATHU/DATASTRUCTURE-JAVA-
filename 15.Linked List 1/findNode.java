0fcf35bc71b4cf59cea4321728b5b759ba07ade1
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-08-30 21:29:36 +0530

    commit :: implemented Find Node

---
+++ 15.Linked List 1/findNode.java
@@ -0,0 +1,107 @@
+/*
+ * Find a Node in Linked List
+Send Feedback
+You have been given a singly linked list of integers. Write a function that returns the index/position of integer data denoted by 'N' (if it exists). Return -1 otherwise.
+Note :
+Assume that the Indexing for the singly linked list always starts from 0.
+Input format :
+The first line contains an Integer 'T' which denotes the number of test cases. 
+
+The first line of each test case or query contains the elements of the singly linked list separated by a single space. 
+
+The second line contains the integer value 'N'. It denotes the data to be searched in the given singly linked list.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence -1 would never be a list element.
+Output format :
+For each test case, return the index/position of 'N' in the singly linked list. Return -1, otherwise.
+
+Output for every test case will be printed in a separate line.
+Note:
+You do not need to print anything; it has already been taken care of. Just implement the given function.
+ Constraints :
+1 <= T <= 10^2
+0 <= M <= 10^5
+
+Where 'M' is the size of the singly linked list.
+
+Time Limit: 1 sec
+Sample Input 1 :
+2
+3 4 5 2 6 1 9 -1
+5
+10 20 30 40 50 60 70 -1
+6
+Sample Output 1 :
+2
+-1
+ Explanation for Sample Output 1:
+In test case 1, 'N' = 5 appears at position 2 (0-based indexing) in the given linked list.
+
+In test case 2, we can see that 'N' = 6 is not present in the given linked list.
+Sample Input 2 :
+2
+1 -1
+2
+3 4 5 2 6 1 9 -1
+6
+Sample Output 2 :
+-1
+4
+ Explanation for Sample Output 2:
+In test case 1, we can see that 'N' = 2 is not present in the given linked list.
+
+In test case 2, 'N' = 6 appears at position 4 (0-based indexing) in the given linked list.
+ */
+
+import java.util.Scanner;
+
+class Node<T> {
+    T data;
+    Node<T> next;
+
+    Node(T data) {
+        this.data = data;
+    }
+}
+
+public class findNode {
+
+    public static int findNode(Node<Integer> head, int ele) {
+        int count = 0;
+        while (head != null) {
+            if (head.data == ele) {
+                return count;
+            }
+            count++;
+            head = head.next;
+        }
+        return  -1 ;
+    }
+
+    public static Node<Integer> takeInput() {
+
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        Node<Integer> head = null, tail = null;
+        while (data != -1) {
+            Node<Integer> currentNode = new Node<Integer>(data);
+            if (head == null) {
+                head = currentNode;
+                tail = currentNode;
+            } else {
+                tail.next = currentNode;
+                tail = tail.next;
+
+            }
+
+            data = sc.nextInt();
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> head = takeInput();
+        int count = findNode(head, 26);
+        System.out.println(count);
+    }
+}
